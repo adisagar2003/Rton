b41fda3a3b9b41a399ee56de6d71461159f0129e
Adi <adityasagarpython@gmail.com>
2024-12-17 23:40:40 -0400

    Improved movement
    Solved Vector sum error, started working on trails.

---
+++ Scenes/trail.gd
@@ -0,0 +1,15 @@
+extends Line2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $".."
+@onready var trail: Line2D = $"."
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var pos = animated_sprite_2d.global_position;
+	add_point(pos);
+	pass
