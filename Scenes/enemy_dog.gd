15590068ec58b6a175a91ae3196aabbf38c7b270
Adi <adityasagarpython@gmail.com>
2024-12-20 15:30:00 -0400

    Added states
    Todo:
    Add screen shake on damaging enemy
    Add patrol state

--- Scenes/enemy_dog.gd
+++ Scenes/enemy_dog.gd
@@ -6,9 +6,16 @@ extends CharacterBody2D
 # health
 @export var health = 20;
 
+# 
+enum State {PATROL, HURT, CHASE, ATTACK}
+var state = State.PATROL;
+var randomLocation = Vector2.ZERO
 @export var SPEED = 100;
 # try to get the player reference
 	
+func _ready() -> void:
+	Patrol();
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	#if not is_on_floor():
@@ -30,14 +37,27 @@ func _physics_process(delta: float) -> void:
 		$Sprite2D.flip_h = true;
 	if (velocity.x > 0):
 		$Sprite2D.flip_h = false;
-	if (player):	
-		
-		position = position.move_toward(player.position,delta*SPEED);
-	
 	move_and_slide()
 	
 func Enemy():
 	pass
 	
+func Patrol():
+	if (state == State.PATROL):
+		pass
+	pass
+	
 func TakeDamage():
 	health -= 10
+
+
+func _on_hurt_box_body_entered(body: Node2D) -> void:
+	if (body.has_method("Player")):
+			var playerState = player.getState();
+			if (playerState == "Attacking"):
+				print("`````````````````````````````Player  Attacked the dog!!!!!```````");
+				state = state.HURT;
+				# apply screenshake fx
+				
+				pass
+	pass # Replace with function body.
